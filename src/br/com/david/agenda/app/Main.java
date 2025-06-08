bc935252d4adb8b4ee81d5753625bcdb39b449d2
DavidOliveira <srnowdkzinx22@gmail.com>
2025-06-08 09:44:58 -0300

    Finalizando Menu(Main)

--- src/br/com/david/agenda/app/Main.java
+++ src/br/com/david/agenda/app/Main.java
@@ -12,35 +12,97 @@ public class Main {
 
         System.out.println("----- Minha Agenda -----");
 
+        Agenda minhaAgenda = new Agenda();
+
         boolean programaFinalizado = false;
         do {
+            pulaLinha();
             System.out.println("Selecione uma das opções abaixo!");
 
-            System.out.println("1. Criar Agenda.");
-            System.out.print("2. Cadastrar novo contato.");
-            System.out.print("3. Editar contato existente.");
-            System.out.print("4. Deletar contato.");
-            System.out.print("5. Listar contatos.");
-            System.out.print("6. Salvar contatos.");
-            System.out.print("7. Carregar contatos.");
-            System.out.print("8. Sair");
+            System.out.println("1. Cadastrar novo contato.");
+            System.out.println("2. Editar contato existente.");
+            System.out.println("3. Deletar contato.");
+            System.out.println("4. Listar contatos.");
+            System.out.println("5. Salvar contatos.");
+            System.out.println("6. Carregar contatos.");
+            System.out.println("7. Sair");
+            pulaLinha();
 
+            System.out.print("Digite: ");
             int opcao = scan.nextInt();
 
-            if (opcao >= 1 && opcao <= 8) {
-
+            scan.nextLine();
                 switch (opcao) {
                     case 1:
+                        System.out.println("Opção 1: Cadastrar novo contato!");
+
+                        System.out.print("Nome: ");
+                        String nome = scan.nextLine();
+
+                        System.out.print("Telefone: ");
+                        String telefone = scan.nextLine();
+
+                        System.out.print("Email: ");
+                        String email = scan.nextLine();
+
+                        minhaAgenda.cadastrarContato(nome, telefone, email);
                         break;
-                    default:
+                    case 2:
+                        System.out.println("Opção 2: Editar contato!");
+
+                        System.out.print("Buscar nome do contato: ");
+                        String nomeAntigo = scan.nextLine();
+
+                        System.out.print("Novo nome: ");
+                        String novoNome = scan.nextLine();
+
+                        System.out.print("Novo telefone: ");
+                        String novoTelefone = scan.nextLine();
+
+                        System.out.print("Novo email: ");
+                        String novoEmail = scan.nextLine();
+                            minhaAgenda.editarContato(nomeAntigo, novoNome, novoTelefone, novoEmail);
+                        break;
+                    case 3:
+                        System.out.println("Opção 3: Excluir contato!");
+
+                        System.out.print("Digite o nome do contato a ser excluido: ");
+                        String nomeExcluido = scan.nextLine();
+                        minhaAgenda.excluirContato(nomeExcluido);
+                        break;
+                    case 4:
+                        System.out.println("Opção 4: Listar contatos!");
+
+                        minhaAgenda.listarContatos();
+                        break;
+                    case 5:
+                        System.out.println("Opção 5: Salvar contatos!");
+
+                        System.out.print("Digite o nome do arquivo para salvar: ");
+                        String nomeArquivo = scan.nextLine();
+
+                        minhaAgenda.salvarContatos(nomeArquivo);
+                        break;
+                    case 6:
+                        System.out.println("Opção 6: Carregar contatos!");
+
+                        System.out.print("Digite o nome do arquivo: ");
+                        String carregarNomeArquivo = scan.nextLine();
+                        minhaAgenda.carregarContatos(carregarNomeArquivo);
+                        break;
+                    case 7:
                         programaFinalizado = true;
                         break;
+                    default:
+                        System.out.println("Dados de entrada inválido, por favor! Entre com opção numérica de 1 a 7, conforme o Menu.");
+                        break;
                 }
-            } else {
-                System.out.println("Dados de entrada inválido, por favor! Entre com opção numérica de 1 a 6, conforme o Menu.");
-            }
 
         } while (!programaFinalizado);
 
+    }
+
+    public static void pulaLinha() {
+        System.out.println();
     }
 }
